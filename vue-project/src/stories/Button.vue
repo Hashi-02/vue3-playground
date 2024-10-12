6147f6c7f3b40eaf06b4e3ffb42c456730daeae5
hashi-02 <hassie0209@gmail.com>
2024-10-13 00:45:51 +0900

    test:

--- vue-project/src/stories/Button.vue
+++ vue-project/src/stories/Button.vue
@@ -1,5 +1,6 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }} </button>
+  <!-- <button type="button" :class="classes" @click="onClick" :style="style">{{ label }} </button> -->
+  <button type="button" :class="classes" :style="style">{{ label }} </button>
 </template>
 
 <script lang="ts" setup>
@@ -41,8 +42,8 @@ const style = computed(() => ({
   backgroundColor: props.backgroundColor
 }));
 
-const onClick = () => {
-  emit("click", 1)
-};
+// const onClick = () => {
+//   emit("click", 1)
+// };
 
 </script>
