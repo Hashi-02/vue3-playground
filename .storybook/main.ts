5eef896323f230a352e568cc39559289b9f13b25
hashi-02 <hassie0209@gmail.com>
2025-01-04 13:22:43 +0900

    fix:ファイル構成を修正する

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/components/**/*.stories.js",
+  ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
